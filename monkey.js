ff6b3d99afe69beb070fa2d8b2439999378758fc
Steve <sewell.steve@gmail.com>
2012-11-18 19:24:13 -0800

    Fixed a bug where once removed the overlay wouldn't come back

--- monkey.js
+++ monkey.js
@@ -96,7 +96,7 @@ define('monkey', function () {
         this.stop();
 
       if (this.options.overlayErrors) {
-        this.$overlayContainer = this.$overlayContainer ||
+        this.$overlayContainer = this.$overlayContainer.is(':visible') ||
           $('<div style="overflow: scroll; position: fixed; top: 0;' +
             'left: 0; bottom: 0; right: 0; background-image:' +
             '-webkit-radial-gradient(circle, rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.6));' +
